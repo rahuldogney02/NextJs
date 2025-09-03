40ecae24f6db4f1466c6453bf0b1edbc28e3d998
rahuldogney02 <rahuldogney02@gmail.com>
2025-09-03 19:20:10 +0530

    initial commit

---
+++ client/frontend/app/page.js
@@ -0,0 +1,17 @@
+'use client'
+export default function Home() {
+
+  console.log("Rendering Home Page");
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <h1>
+          Main Section
+        </h1>
+      </main>
+      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+        <h2 onClick={() => console.log("Footer Section Clicked")}>Footer Section</h2>
+      </footer>
+    </div>
+  );
+}
